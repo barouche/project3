a560126a5d78b063d9296fe2c8c134ce5fa63247
Daniel Papastratidis <idanpap@gmail.com>
2020-10-07 10:14:38 +0200

    added build stuff

---
+++ client/build/precache-manifest.fb4681045921419e419a47a5beeedd51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f0d83f92c57ecc480abbcc72184d02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b28f26b3dbf1f2cd574",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "ef00c02bae1811757789",
+    "url": "/static/css/main.7e1bfac0.chunk.css"
+  },
+  {
+    "revision": "9b28f26b3dbf1f2cd574",
+    "url": "/static/js/2.84607359.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.84607359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef00c02bae1811757789",
+    "url": "/static/js/main.be2497e4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
