d99072cd59123b802089f753cda0d6cea633ee1e
Daniel Papastratidis <idanpap@gmail.com>
2020-10-08 11:51:42 +0200

    push new build

---
+++ client/build/precache-manifest.e84bef12693f63586787ee43efdd8864.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a35834d143358c172470d711e89b5b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a864249b9a0d170332b",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "182f02beb884c4923061",
+    "url": "/static/css/main.f4f89269.chunk.css"
+  },
+  {
+    "revision": "8a864249b9a0d170332b",
+    "url": "/static/js/2.ec0b98da.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.ec0b98da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "182f02beb884c4923061",
+    "url": "/static/js/main.86ab854b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "29f71dfaac240bc3368a53001b826f01",
+    "url": "/static/media/logo.29f71dfa.png"
+  }
+]);
